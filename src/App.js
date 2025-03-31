eadfe7757f88431874b679097d2f7f0955041b27
mas117 <sitnikovamasha2004@gmail.com>
2025-03-31 13:35:02 +0700

    Main page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/footer/footer";
+import Body from './components/body/body'; 
+
+function App() {
+  return (
+     <>
+    <div style={{display:'flex', justifyContent: 'center'}}>
+    <Body/>
+    </div>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
