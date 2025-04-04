a88532ade6f3d9defc2721686360baecd63e2940
ArinaBulygina <bulyginaarina2@gmail.com>
2025-04-04 21:20:33 +0700

    Add new architecture

--- src/index.js
+++ src/index.js
@@ -1,121 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './layout/Header.js';
-import Search from './components/Search/Search.js';
-import DropDownSort from './components/DropDownSort/DropDownSort.js';
-import DropDownCategories from './components/DropDownCategories/DropDownCategories.js';
-import Catalog from './components/Catalog/Catalog.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Catalog_page from './Catalog_page/Catalog_page.js';
+import Details_page from './Details_page/Details_page.js';
 
 function App(){
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [categories, setCategories] = useState([]);
-
-  async function loadData()
-  {
-    setLoading(true);
-    try {
-      const [response1, response2] = await Promise.all([
-        fetch("catalog/books"), //дописать!! (все книги)
-        fetch("catalog/categories/") // дописать !! (список категорий)
-      ]);
-
-      const [data1, data2] = await Promise.all([
-        response1.json(),
-        response2.json()
-      ]);
-
-      setBooks(data1);
-      setCategories(data2);
-    }
-    catch (err) {
-      setError("Ошибка загрузки данных");
-    }
-    finally
-    {
-      setLoading(false);
-    }
-  };
-
-  async function editBooks(parameter)
-  {
-    setLoading(true);
-    try{
-      let url = `catalog/books/sorted/?category=${parameter}`; // дописать!!!!
-      if (parameter == "asc" || parameter == "desc")
-      {
-        url = `catalog/books/sorted/?ordering=${parameter === "asc" ? "price" : "-price"}` // дописать!!!!
-      }
-      const response = await fetch(url);
-      const data = await response.json();
-      setBooks(data);
-    } 
-    catch (err) {
-      setError("Ошибка загрузки данных");
-    }
-    finally
-    {
-      setLoading(false);
-    }
-  };
-
-  async function editBooksSearch(query)
-  {
-    setLoading(true);
-    try{
-      let url = "catalog/books/search";
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query }),
-      });
-      const data = await response.json();
-      setBooks(data);
-    } 
-    catch (err) {
-      setError("Ошибка загрузки данных");
-    }
-    finally
-    {
-      setLoading(false);
-    }
-  };
   
-  useEffect (() => {
-    loadData();
-  }, []);
-
-  const handleChange = (parameter) => {
-    editBooks(parameter);
-  };
-
-  const handleSearchChange = (query) => {
-    editBooksSearch(query);
-  };
-
-  if (loading)
-  {
-    return <p>"Загрузка ..."</p>
-  }
-
-
-
   return(
-    <div style={{height: '100%'}}>
-      <Header />
-      <div style={{display:'flex', alignItems: 'center', marginBottom: 42}}>
-        <Search onSearchChange={handleSearchChange}/>
-        <DropDownSort onSortChange={handleChange} />
-        <DropDownCategories allCategories={categories} onCategoriesChange={handleChange} />
-      </div>
-      {error ? (<p style={{fontSize: 20, color: 'lightgray'}}>{error}</p>):  (
-      books.length > 0 ? 
-        (<Catalog data={books} />) : 
-        (<p style={{fontSize: 20, color: 'lightgray'}}>Похоже, у нас такого нет</p>))}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Catalog_page />} />
+        <Route path="/new" element={<Details_page />} />
+      </Routes>
+    </Router>
   );
 };
 
@@ -125,4 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
