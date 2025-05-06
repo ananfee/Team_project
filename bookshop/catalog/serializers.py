9da85037ce988e4f04d6e4ce13e752ff57658c82
Nastya Lunenok <nastya.lunenok@gmail.com>
2025-05-06 17:23:39 +0700

    Edit OrderHistorySerializer

--- bookshop/catalog/serializers.py
+++ bookshop/catalog/serializers.py
@@ -172,7 +172,7 @@ class BookInOrderSerializer(serializers.ModelSerializer):
 
 class OrderHistorySerializer(serializers.ModelSerializer):
     books = BookInOrderSerializer(many=True, read_only=True, source='bookinorder_set', context={'request': None})
-    status_name = serializers.CharField(source='status.status_name', read_only=True)
+    status_name = serializers.CharField(source='status.name_status', read_only=True)
     class Meta:
         model = OrderHistory
         fields = ['id', 'sale_date', 'sale_price', 'status_name', 'books']
