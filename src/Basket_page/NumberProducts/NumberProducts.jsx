2a27aa4f916e6b3f652efb1ddc1cf1fc168b8c09
mas117 <sitnikovamasha2004@gmail.com>
2025-05-18 22:37:06 +0700

    Logic Basket Page

--- src/Basket_page/NumberProducts/NumberProducts.jsx
+++ src/Basket_page/NumberProducts/NumberProducts.jsx
@@ -3,15 +3,32 @@ import "./NumberProducts.css";
 import ac from '../../images/add-circle.png';
 import mc from '../../images/minus-cirlce.png';
 
-const NumberProducts = () => {
-    const [count, setCount] = useState(1);
+// Компонент теперь принимает initialCount как пропс
+const NumberProducts = ({ initialCount = 1 }) => { // Устанавливаем значение по умолчанию 1, если initialCount не передан или undefined
+    // Используем initialCount для установки начального состояния
+    // Math.max(1, ...) гарантирует, что начальное количество не будет меньше 1
+    const [count, setCount] = useState(Math.max(1, initialCount));
+
+    // Если initialCount может меняться после первого рендера, и вы хотите,
+    // чтобы компонент NumberProducts обновлялся, нужно использовать useEffect:
+    /*
+    useEffect(() => {
+        setCount(Math.max(1, initialCount));
+    }, [initialCount]);
+    // Если initialCount не меняется после загрузки данных корзины,
+    // этот useEffect не обязателен и можно обойтись просто useState(Math.max(1, initialCount));
+    */
+
 
     const handleIncrement = () => {
         setCount(count + 1);
+        // TODO: Возможно, здесь нужно вызвать callback для уведомления родителя (ProductInBasket или выше)
+        // об изменении количества, чтобы обновить общую стоимость позиции и корзины.
     };
 
     const handleDecrement = () => {
         setCount(Math.max(1, count - 1)); // Не даем уменьшить количество ниже 1
+         // TODO: Возможно, здесь нужно вызвать callback для уведомления родителя
     };
 
     return (
@@ -20,7 +37,7 @@ const NumberProducts = () => {
                 <img src={ac} alt="Увеличить" />
             </button>
             <div className='NumberProducts'>
-                <p>{count}</p>
+                <p>{count}</p> {/* Отображаем текущее состояние count */}
             </div>
             <button className='rectangleMinuss' onClick={handleDecrement}>
                 <img src={mc} alt="Уменьшить" />
@@ -30,4 +47,3 @@ const NumberProducts = () => {
 };
 
 export default NumberProducts;
-
