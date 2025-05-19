a133ef600e59690b4c5054d353d29abc8af08f0c
Nastya Lunenok <nastya.lunenok@gmail.com>
2025-05-19 21:56:42 +0700

    Edit OrderHistorySerializer

--- bookshop/catalog/serializers.py
+++ bookshop/catalog/serializers.py
@@ -164,7 +164,7 @@ class RemoveCartItemSerializer(serializers.Serializer):
 
 class BookInOrderSerializer(serializers.ModelSerializer):
     book_id = serializers.IntegerField(source='book.id', read_only=True)
-    cover_image = serializers.SerializerMethodField()
+    cover_image = serializers.ImageField(source='book.cover_image', read_only=True)
     title = serializers.CharField(source='book.title', read_only=True)
     class Meta:
         model = BookInOrder
