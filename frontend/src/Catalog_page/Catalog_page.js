5b368ecabe9e3ce5153eae1166bb5bf16bbae1af
HUAWEI_POLINA\Shevt <shevtsovapolina2004@yandex.ru>
2025-05-21 22:47:53 +0700

    edit rout

--- frontend/src/Catalog_page/Catalog_page.js
+++ frontend/src/Catalog_page/Catalog_page.js
@@ -33,8 +33,8 @@ function Catalog_page()
     setLoading(true);
     try {
       const [response1, response2] = await Promise.all([
-        fetch("http://5.129.207.153/api/catalog/books/"),
-        fetch("http://5.129.207.153/api/catalog/categories/")
+        fetch("http://backend:8001/catalog/books/"),
+        fetch("http://backend:8001/catalog/categories/")
       ]);
 
       const [data1, data2] = await Promise.all([
@@ -57,7 +57,7 @@ function Catalog_page()
   async function editBooks(filterOptions) {
     setLoading(true);
     try {
-      let url = "http://5.129.207.153/api/catalog/books/sorted/";
+      let url = "http://backend:8001/catalog/books/sorted/";
       const params = new URLSearchParams();
   
       if (filterOptions.category) {
@@ -82,7 +82,7 @@ function Catalog_page()
   async function editBooksSearch(query)
   {
     try{
-      let url = `http://5.129.207.153/api/catalog/books/search/?q=${query}`;
+      let url = `http://backend:8001/catalog/books/search/?q=${query}`;
       const response = await fetch(url);
       const data = await response.json();
       setBooks(data);
