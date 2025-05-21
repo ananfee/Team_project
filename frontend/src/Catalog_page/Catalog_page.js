6840386b418642ef9b4b83ee3bf1d9a5fdbe80c1
HUAWEI_POLINA\Shevt <shevtsovapolina2004@yandex.ru>
2025-05-21 23:15:26 +0700

    edit rout

--- frontend/src/Catalog_page/Catalog_page.js
+++ frontend/src/Catalog_page/Catalog_page.js
@@ -33,8 +33,8 @@ function Catalog_page()
     setLoading(true);
     try {
       const [response1, response2] = await Promise.all([
-        fetch("http://backend:8000/catalog/books/"),
-        fetch("http://backend:8000/catalog/categories/")
+        fetch("http://5.129.207.153:8001/catalog/books/"),
+        fetch("http://5.129.207.153:8001/catalog/categories/")
       ]);
 
       const [data1, data2] = await Promise.all([
