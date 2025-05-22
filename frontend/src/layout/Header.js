1324e63d9c7d004b62226fe0f5088e62a940d7a9
ArinaBulygina <bulyginaarina2@gmail.com>
2025-05-23 01:44:51 +0700

    Add logic for catalog for admin

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"; // Импортируем Link
+import { Link } from "react-router-dom"; 
 import styles from "../layout/Header.module.css";
 import NotificationButton from "./NotificationButton/NotificationButton.js";
 import OrdersButton from "./OrdersButton/OrdersButton.jsx";
-import BasketButton from "./BasketButton/BasketButton.jsx";
 import LoginWindow from "./LoginWindow/LoginWindow.js";
+import FetchWithAuth from "./LoginWindow/FetchWithAuth.js";
+
 
 function Header({onOpenModal}) {
    const [loginModalOpen, setLoginModalOpen] = useState(false);
@@ -19,7 +20,7 @@ function Header({onOpenModal}) {
     const handleLogout = async () => {
       const refreshToken = localStorage.getItem('refreshToken');
       try {
-         const response = await fetch('http://5.129.207.153:8001/catalog/logout/', {
+         const response = await FetchWithAuth('http://127.0.0.1:8000/catalog/logout/', {
             method: 'POST',
             headers: {
                'Content-Type': 'application/json'
@@ -65,7 +66,9 @@ function Header({onOpenModal}) {
                      <>
                         <NotificationButton onClick={onOpenModal} />
                         <OrdersButton />
-                        <BasketButton />
+                        <button className={styles.deliveryButton}>
+                        <img src="shopping-cart.svg" alt="корзина" />
+                        </button>
                         <button className={styles.Role}>Покупатель</button>
                      </>
                   )
@@ -73,13 +76,16 @@ function Header({onOpenModal}) {
                      <>
                         <NotificationButton onClick={onOpenModal} />
                         <OrdersButton />
-                        <BasketButton />
+                        <button className={styles.deliveryButton}>
+                        <img src="shopping-cart.svg" alt="корзина" />
+                        </button>
                      </>
                   )
                }
             {isAuth 
                ? <button className={styles.loginButton} onClick={handleLogout}>Выйти</button>
-               : <button className={styles.loginButton} onClick={() => setLoginModalOpen(true)}>Войти</button>
+               : 
+               <button className={styles.loginButton} onClick={() => setLoginModalOpen(true)}>Войти</button>
             }
             <LoginWindow isOpen={loginModalOpen} onClose={() => setLoginModalOpen(false)} />
          </div>
