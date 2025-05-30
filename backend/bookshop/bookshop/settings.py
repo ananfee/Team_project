a7067b27bfd69fc7b04bd26645ad08ce7f9d7e00
HUAWEI_POLINA\Shevt <shevtsovapolina2004@yandex.ru>
2025-05-30 20:41:43 +0700

    edit signals

--- backend/bookshop/bookshop/settings.py
+++ backend/bookshop/bookshop/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'catalog.apps.CatalogConfig',
+
 ]
 
 SIMPLE_JWT = {
