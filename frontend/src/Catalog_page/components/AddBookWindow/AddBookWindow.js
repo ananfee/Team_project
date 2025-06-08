77bddecd63e77cd92f6c37ef5df6cdbc036ad7fe
ArinaBulygina <bulyginaarina2@gmail.com>
2025-06-09 01:07:34 +0700

    Fixed errors

--- frontend/src/Catalog_page/components/AddBookWindow/AddBookWindow.js
+++ frontend/src/Catalog_page/components/AddBookWindow/AddBookWindow.js
@@ -175,8 +175,11 @@ function AddBookWindow({isOpen, onClose, obj})
       formData.append('number_of_copies', Number(count));
       formData.append('description', description);
       formData.append('authors_data_json', JSON.stringify(authors));
-      formData.append('cover_image', imgFile);
       
+      if (imgFile) {
+         formData.append('cover_image', imgFile);
+      }
+        
        const isEdit = !!obj; 
   
        const url = isEdit
--- frontend/src/layout/LoginWindow/LoginWindow.js
+++ frontend/src/layout/LoginWindow/LoginWindow.js
@@ -211,7 +211,6 @@ function LoginWindow ({isOpen, onClose})
           localStorage.setItem('totalItems', totalItems);
           window.dispatchEvent(new Event('cartUpdated'));
           window.location.reload(); 
-
         }
       } catch (e) {
         alert('Ошибка отправки запроса');
@@ -252,7 +251,7 @@ function LoginWindow ({isOpen, onClose})
         <p className={styles.pEntrance}>
           {isRegistering ? "Регистрация" : "Вход"}
         </p>
-        <form onSubmit={isRegistering ? handleRegistering : handleLogin}>
+        <form onSubmit={isRegistering ? handleRegistering : handleLogin()}>
           <div className={styles.InputContainer}>
           <div className={styles.InputContainerInner}>
             
