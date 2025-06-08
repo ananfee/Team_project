755ec1aebfc9f0f1ac5c6f75b71e19a2681e8a74
ArinaBulygina <bulyginaarina2@gmail.com>
2025-06-08 20:03:19 +0700

    Fixed errors

--- frontend/src/Catalog_page/components/AddBookWindow/AddBookWindow.js
+++ frontend/src/Catalog_page/components/AddBookWindow/AddBookWindow.js
@@ -41,7 +41,7 @@ function AddBookWindow({isOpen, onClose, obj})
    const fetchDropdownData = async () => {
       // Авторы
       try {
-        const response = await FetchWithAuth(`${baseUrl}catalog/authors/`);
+        const response = await FetchWithAuth('http://127.0.0.1:8000/catalog/authors/');
         const authors = await response.json();
         setLast_name(authors.last_names || []);
         setFirst_names(authors.first_names || []);
@@ -175,11 +175,8 @@ function AddBookWindow({isOpen, onClose, obj})
       formData.append('number_of_copies', Number(count));
       formData.append('description', description);
       formData.append('authors_data_json', JSON.stringify(authors));
+      formData.append('cover_image', imgFile);
       
-      if (imgFile) {
-         formData.append('cover_image', imgFile);
-      }
-        
        const isEdit = !!obj; 
   
        const url = isEdit
