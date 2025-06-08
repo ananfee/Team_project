b07f32ffa56487d240be1e94efdae689c397783b
ArinaBulygina <bulyginaarina2@gmail.com>
2025-06-09 01:07:34 +0700

    Fixed errors

--- frontend/src/Catalog_page/components/AddBookWindow/AddBookWindow.js
+++ frontend/src/Catalog_page/components/AddBookWindow/AddBookWindow.js
@@ -177,13 +177,8 @@ function AddBookWindow({isOpen, onClose, obj})
       if (imgFile) {
          formData.append('cover_image', imgFile);
       }
-      
-      
+        
        const isEdit = !!obj; 
-       
-      if (isEdit) {
-         formData.append('cover_image', imgFile);
-      }
   
        const url = isEdit
          ? `${baseUrl}catalog/books/update/${obj.id}/`
--- frontend/src/layout/LoginWindow/LoginWindow.js
+++ frontend/src/layout/LoginWindow/LoginWindow.js
@@ -212,7 +212,6 @@ function LoginWindow ({isOpen, onClose})
           localStorage.setItem('totalItems', totalItems);
           window.dispatchEvent(new Event('cartUpdated'));
           window.location.reload(); 
-
         }
       } catch (e) {
         alert('Ошибка отправки запроса');
@@ -253,7 +252,7 @@ function LoginWindow ({isOpen, onClose})
         <p className={styles.pEntrance}>
           {isRegistering ? "Регистрация" : "Вход"}
         </p>
-        <form onSubmit={isRegistering ? handleRegistering : handleLogin}>
+        <form onSubmit={isRegistering ? handleRegistering : handleLogin()}>
           <div className={styles.InputContainer}>
           <div className={styles.InputContainerInner}>
             
