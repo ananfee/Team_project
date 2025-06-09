b55fc61466603d9db655d900fd65fa0a46d5d256
HUAWEI_POLINA\Shevt <shevtsovapolina2004@yandex.ru>
2025-06-09 11:33:12 +0700

    edit front

--- frontend/src/layout/LoginWindow/LoginWindow.js
+++ frontend/src/layout/LoginWindow/LoginWindow.js
@@ -252,7 +252,7 @@ function LoginWindow ({isOpen, onClose})
         <p className={styles.pEntrance}>
           {isRegistering ? "Регистрация" : "Вход"}
         </p>
-        <form onSubmit={isRegistering ? handleRegistering : handleLogin()}>
+        <form onSubmit={isRegistering ? handleRegistering : handleLogin}>
           <div className={styles.InputContainer}>
           <div className={styles.InputContainerInner}>
             
